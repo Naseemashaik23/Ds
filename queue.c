9bb5214cf51aa41172b74943fe1dfc424e5ebea3
Naseemashaik23 <166896243+Naseemashaik23@users.noreply.github.com>
2024-04-30 10:51:08 +0530

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,33 +1,40 @@
-\* c program to implement stacks using array*\
+
 #include<stdio.h>
-int main()
+#include<stdlib.h>
 #define MAX 20
-int queue[MAX]
+int queue[MAX];
 int front=-1,rear=-1;
-void insert();
+void insert(int);
 void delete();
 void display();
 int main()
 {
 
-        int choice;
-        printf("insert element
-        printf("enter the element:");
-        scanf("%d",&item);
-        switch(choice)
-        {
-                case 1: printf("enter the element:\n");
-                        scanf("%d",item);
-                        insert(item);
-                        break;
-                case 2: delete();
-                        break;
-                case 3: display();
-                        break;
-                case 4: exit(0);
-                default:("invalid choice:\n");
-      }
-                                              
+        int choice,item;
+        while(1){
+        
+		printf("\n menu\n");
+		printf("insert element\n");
+		printf("delete element\n");
+		printf("display element\n");
+		printf("exit\n");
+		printf("enter the element:");
+		scanf("%d",&item);
+		switch(choice)
+		{
+		        case 1: printf("enter the element:\n");
+		                scanf("%d",&item);
+		                insert(item);
+		                break;
+		        case 2: delete();
+		                break;
+		        case 3: display();
+		                break;
+		        case 4: exit(0);
+		        default:("invalid choice:\n");
+	       }
+	  }
+
 }
 void insert(int item)
 {
@@ -43,12 +50,25 @@ void insert(int item)
 void delete()
 {
         if(front==-1)
-                printf("queue is empty:\n);
+                printf("queue is empty:\n");
         else
         {
-                printf("delete element:\n");
+                printf("delete element:%d\n",queue[front]);
                 if(front==rear)
                         front=rear=-1;
                 else
-                {
-                        printf
+         		 front++;
+         }
+}
+void dispay()
+{
+	int i;
+	if(front==-1)
+		printf("queue is empty\n");
+	else
+	{
+		printf("queue content\n");
+		for(i=front;i<=rear;i++)
+			printf("%4d",queue[i]);
+	}
+}
